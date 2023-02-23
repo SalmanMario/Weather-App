cbd996f84981d90faeff471a45630f64d5e8dd2a
Mario <xmariosalman@gmail.com>
2023-02-23 20:33:29 +0200

    added scroll to top button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
 let currentCity = localStorage.getItem("city");
 console.log("Current City", currentCity);
 
